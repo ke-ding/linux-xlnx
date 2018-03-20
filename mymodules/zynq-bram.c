5ae34b79ff2a22104b2e6e8e065fa43e1a09edbe
milo <milod@163.com>
2018-03-20 17:28:36 +0800

    multiply bram block access, test ok!

--- mymodules/zynq-bram.c
+++ mymodules/zynq-bram.c
@@ -6,38 +6,99 @@
 #include <linux/pagemap.h>
 #include <linux/device.h>
 #include <linux/of_address.h>
+#include <linux/interrupt.h>
 #include <linux/platform_device.h>
 
 #define __MILO_DEBUG__
 
-static int l_bram_major = 0;
-static u32 l_mem = 0;
-static u32 l_phy_addr = 0;
-static resource_size_t l_size = 0;
+#define DEV_NAME	"bram_map"
+#define MAX_DEV_NO	8
+
+typedef struct {
+	u32 phy_addr;
+	resource_size_t size;
+	int irq;
+
+	u32 mem;
+
+	wait_queue_head_t read_wq;
+	int data_avail;
+}bram_info_t;
+
+static bram_info_t bram_info[MAX_DEV_NO];
+static int bram_major = -1;
+static int dev_no = 0;
+
+static irqreturn_t bram_interrupt(int irq, void *dev_id)
+{
+	bram_info_t *info = (bram_info_t *)dev_id;
+
+	info->data_avail = 1;
+	wake_up_interruptible(&info->read_wq);
+
+	return IRQ_HANDLED;
+}
 
 static int bram_open(struct inode *inode, struct file *file)
 {
+	const int minor = iminor(inode);
+	int err = 0;
+	char intr_name[255];
+
 	printk(KERN_DEBUG"(MILO)bram_open\n");
-	return 0;
+
+	file->private_data = &bram_info[minor];
+
+	if (bram_info[minor].irq > 0) {
+		sprintf(intr_name, "bram_intr%d", minor);
+		err = request_irq(bram_info[minor].irq, bram_interrupt,
+				IRQF_SHARED | IRQF_TRIGGER_RISING,
+				intr_name,
+				&bram_info[minor]);
+		if (err) {
+			printk(KERN_ERR"(MILO)request interrupt err (%d)\n", err);
+		}
+	}
+
+	return err;
 }
 
 static int bram_close(struct inode *inode, struct file *file)
 {
+	const int minor = iminor(inode);
+
 	printk(KERN_DEBUG"(MILO)bram_close\n");
+
+	if (bram_info[minor].irq > 0) {
+		free_irq(bram_info[minor].irq, 0);
+	}
+
+	return 0;
+}
+
+static ssize_t bram_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
+{
+	bram_info_t *info = (bram_info_t *)file->private_data;
+
+	wait_event_interruptible(info->read_wq, info->data_avail == 1);
+
+	info->data_avail = 0;
+
 	return 0;
 }
 
 static int bram_mmap(struct file *file, struct vm_area_struct * vma)
 {
+	bram_info_t *info = (bram_info_t *)file->private_data;
 	size_t size = vma->vm_end - vma->vm_start;
 
-	if (size > l_size)
+	if (size > info->size)
 		return -EINVAL;
 
 	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 	if (remap_pfn_range(vma,
 				vma->vm_start,
-				l_phy_addr >> PAGE_SHIFT,
+				info->phy_addr >> PAGE_SHIFT,
 				size,
 				vma->vm_page_prot))
 		return -EAGAIN;
@@ -50,40 +111,94 @@ static struct file_operations bram_fops = {
 	.llseek		= no_llseek, 
 	.open		= bram_open,
 	.release	= bram_close,
+	.read		= bram_read,
 	.mmap		= bram_mmap,
 };              
 
-static struct class *bram_class;
+static struct class *bram_class = NULL;
 
 static int zynq_bram_probe(struct platform_device *pdev)
 {
 	struct resource *res;
+	int err = -1;
+
+	if (dev_no >= MAX_DEV_NO) {
+		printk(KERN_ERR"(MILO)too many %s devices\n", DEV_NAME);
+		goto err_out;
+	}
+
+	init_waitqueue_head(&bram_info[dev_no].read_wq);
+
+	bram_info[dev_no].irq = platform_get_irq(pdev, 0);
+	if (bram_info[dev_no].irq < 0)
+		bram_info[dev_no].irq = -1;
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	l_size = resource_size(res);
-	l_phy_addr = res->start;
-	l_mem = (u32) devm_ioremap(&pdev->dev, res->start, l_size);
-	if (IS_ERR((void *)l_mem))
-		return PTR_ERR((void *)l_mem);
+	bram_info[dev_no].size = resource_size(res);
+	bram_info[dev_no].phy_addr = res->start;
+	bram_info[dev_no].mem = (u32) devm_ioremap(&pdev->dev,
+						bram_info[dev_no].phy_addr,
+						bram_info[dev_no].size);
+	if (IS_ERR((void *)bram_info[dev_no].mem))
+		return PTR_ERR((void *)bram_info[dev_no].mem);
 
-	l_bram_major = register_chrdev(0, "bram_map", &bram_fops);
-	bram_class = class_create(THIS_MODULE, "bram_map");
+	if (bram_major < 0) {
+		bram_major = register_chrdev(0, DEV_NAME, &bram_fops);
+		if (bram_major < 0) {
+			err = bram_major;
+			goto reg_err;
+		}
+		bram_class = class_create(THIS_MODULE, DEV_NAME);
+		if (IS_ERR(bram_class)) {
+			err = PTR_ERR(bram_class);
+			bram_class = NULL;
+			goto class_err;
+		}
+	}
+
 	device_create(bram_class, NULL,
-			MKDEV(l_bram_major, 0),
+			MKDEV(bram_major, dev_no),
 			NULL,
-			"bram_map");
+			DEV_NAME"%d",
+			dev_no);
+	printk(KERN_INFO"(MILO)ZYNQ blk-ram Mapper driver loaded\n"
+			"\tminor: %d\n"
+			"\tphy addr: 0x%08x\n"
+			"\tsize: 0x%04x\n"
+			"\tirq: %d\n",
+			dev_no,
+			bram_info[dev_no].phy_addr,
+			bram_info[dev_no].size,
+			bram_info[dev_no].irq);
 
-	printk(KERN_INFO "ZYNQ blk-ram Mapper driver loaded\n");
+	++dev_no;
+
 
 	return 0;
+
+class_err:
+	unregister_chrdev(bram_major, DEV_NAME);
+reg_err:
+	printk(KERN_ERR"(MILO)failed to register %s device (%d)\n",
+			DEV_NAME, err);
+err_out:
+	return err;
 }
 
 static int zynq_bram_remove(struct platform_device *pdev)
 {
-	device_destroy(bram_class, MKDEV(l_bram_major, 0));
-	class_destroy(bram_class);
+	if (bram_class) {
+		--dev_no;
+		while (dev_no > 0) {
+			device_destroy(bram_class, MKDEV(bram_major, dev_no));
+			--dev_no;
+		}
 
-	unregister_chrdev(l_bram_major, "bram_map");
+		class_destroy(bram_class);
+	}
+
+	if (bram_major > 0)
+		unregister_chrdev(bram_major, DEV_NAME);
 
 	return 0;
 }
